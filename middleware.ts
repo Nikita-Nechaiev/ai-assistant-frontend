eaf988edbdcc987fe658a66701b5bfadec6a06fe
Nikita-Nechaiev <nikita.nechaiev.dev@gmail.com>
2025-02-16 22:23:02 +0100

    middleware logs

--- middleware.ts
+++ middleware.ts
@@ -10,6 +10,7 @@ export async function middleware(req: NextRequest) {
     url.pathname === '/forgot-password' ||
     url.pathname.startsWith('/reset-password')
   ) {
+    console.log('authPage')
     const accessToken = req.cookies.get('accessToken')?.value;
     if (accessToken) {
       url.pathname = '/dashboard';
@@ -18,6 +19,7 @@ export async function middleware(req: NextRequest) {
     return NextResponse.next();
   }
   const refreshToken = req.cookies.get('refreshToken')?.value;
+  console.log('here1', refreshToken)
 
   if (!refreshToken) {
     const loginUrl = req.nextUrl.clone();
@@ -32,32 +34,37 @@ export async function middleware(req: NextRequest) {
   const refreshUrl = `${process.env.NEXT_PUBLIC_API_URL}/auth/get-tokens`;
 
   try {
+    console.log('here2')
     const refreshResponse = await axios.get(refreshUrl, {
       withCredentials: true,
       headers: {
         Cookie: `refreshToken=${refreshToken}`,
       },
     });
+    console.log('here3')
     if (refreshResponse.status === 200) {
       const { accessToken, newRefreshToken, user } = refreshResponse.data;
 
       const res = NextResponse.next();
+      console.log('here4', accessToken, refreshResponse, user)
 
       res.cookies.set('accessToken', accessToken, {
         httpOnly: true,
         secure: true,
         maxAge: 15 * 60,
-        sameSite: 'none',
+        sameSite: 'strict',
       });
       res.cookies.set('refreshToken', newRefreshToken, {
         httpOnly: true,
         secure: true,
         maxAge: 30 * 24 * 60 * 60, // 30 days in seconds
-        sameSite: 'none',
+        sameSite: 'strict',
       });
       res.headers.set('x-user', JSON.stringify(user));
       return res;
     }
+
+    console.log('here5')
 
     const failUrl = req.nextUrl.clone();
     failUrl.pathname = '/login';
@@ -70,7 +77,7 @@ export async function middleware(req: NextRequest) {
     if (error instanceof AxiosError) {
       console.log('Error refreshing token:', error.message);
     }
-
+    console.log('here6')
     const errUrl = req.nextUrl.clone();
     errUrl.pathname = '/login';
 
